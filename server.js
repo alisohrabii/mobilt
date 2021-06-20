f74f18467aebce7a9de2ae1132a1b234d9bc890b
alisoh <sornasoh50@gmail.com>
2021-06-20 22:25:38 +0430

    ghhfdh

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 const Product = require('./model/product');
 const config = require("./config/keys");
 const path=require('path');
-require("dotenv").config({path:"./config.env"});
+//require("dotenv").config({path:"./config.env"});
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI ,
+mongoose.connect(config.mongoURI ,
   {
     useNewUrlParser: true, useUnifiedTopology: true,
     useCreateIndex: true, useFindAndModify: false
@@ -26,22 +26,15 @@ app.listen(port, () => {
 
 
 if(process.env.NODE_ENV=='production'){
-  app.use(express.static(path.join(__dirname,'/client/build')));
+  app.use(express.static('client/build'));
   app.get('*',(req,res)=>{
 
-    res.sendFile(path.join(__dirname,'client','build','index.html'));
-  })
-
-
-}else{
-
-  app.get('/',(req,res)=>{
-
-    res.send('hellllloooooooe');
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   })
 
 
 }
+
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("./uploads"));
