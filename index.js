6ce14f0635ad454b4b14f88fa94305b6c7cb766c
alisoh <sornasoh50@gmail.com>
2021-06-21 08:35:26 +0430

    hkhkhsjsj

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 const Product = require('./model/product');
 const config = require("./config/keys");
 const path=require('path');
-//require("dotenv").config({path:"./config.env"});
+require("dotenv").config({path:"./config.env"});
 const mongoose = require("mongoose");
 
-mongoose.connect(config.mongoURI ,
+mongoose.connect(process.env.MONGO_URI ,
   {
     useNewUrlParser: true, useUnifiedTopology: true,
     useCreateIndex: true, useFindAndModify: false
@@ -24,6 +24,13 @@ app.listen(port, () => {
   .catch(err => console.log(err));
 
 
+  app.use(cors());
+  app.use(express.json());
+  app.use("/uploads", express.static("./uploads"));
+  app.use('/users',require('./routes/users'));
+  
+  app.use('/product',require('./routes/product'));
+    
 
 if(process.env.NODE_ENV=='production'){
   app.use(express.static('client/build'));
@@ -34,11 +41,3 @@ if(process.env.NODE_ENV=='production'){
 
 
 }
-
-app.use(cors());
-app.use(express.json());
-app.use("/uploads", express.static("./uploads"));
-app.use('/users',require('./routes/users'));
-
-app.use('/product',require('./routes/product'));
-  
