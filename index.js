b00bbc6251f593051b0cd0f1acdabd9bb10274ab
alisoh <sornasoh50@gmail.com>
2022-04-25 17:38:57 +0430

    mobilt commit

--- index.js
+++ index.js
@@ -6,38 +6,30 @@ const config = require("./config/keys");
 const path=require('path');
 require("dotenv").config({path:"./config.env"});
 const mongoose = require("mongoose");
-
+const hostname ='0.0.0.0';
 mongoose.connect(process.env.MONGO_URI ,
   {
     useNewUrlParser: true, useUnifiedTopology: true,
     useCreateIndex: true, useFindAndModify: false
   })
   .then(() =>{
-    const port =process.env.PORT || 8088;
-app.listen(port, () => {
+    const port = 8088;
+app.listen(port,hostname, () => {
   console.log(`Server Listening on ${port}`)
 })
-  
-    console.log('MongoDB Connected...')
-    
-})
+    console.log('MongoDB Connected...')})
   .catch(err => console.log(err));
-
-
   app.use(cors());
   app.use(express.json());
   app.use("/uploads", express.static("./uploads"));
   app.use('/users',require('./routes/users'));
-  
   app.use('/product',require('./routes/product'));
-    
+//if(process.env.NODE_ENV=='production'){
+ // app.use(express.static('client/build'));
+  //app.get('*',(req,res)=>{
 
-if(process.env.NODE_ENV=='production'){
-  app.use(express.static('client/build'));
-  app.get('*',(req,res)=>{
-
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-  })
+  //  res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  //})
 
 
-}
+//}
